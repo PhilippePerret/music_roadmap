550f84d26fc0a00a438519747f15c549b976550c
PhilippePerret <philippe.perret@yahoo.fr>
2013-04-27 13:48:17 +0200

    Minor changes

--- javascript/locale/fr/message.js
+++ javascript/locale/fr/message.js
@@ -9,19 +9,20 @@ window.MESSAGE = {
   },
   // ROADMAP
   Roadmap:{
-    must_signin_to_create :"Vous devez être identifié (ou inscrit) pour ouvrir ou créer une feuille de route.",
-    how_to_make_a_good_nom: "Pour un nom valide, n'utilisez ni espace, ni caractères spéciaux. Si vous voulez ouvrir une de vos feuilles de route, utilisez le menu, plutôt.",
-    creating    : "Création de la feuille de route…",
-    saving      : "Sauvegarde en cours…",
-    created     : "Feuille de route créée avec succès !",
-    loaded      : "Feuille de route ouverte avec succès !",
-    saved       : "Feuille de route sauvée avec succès !",
-    ready       : "La nouvelle feuille de route est prête",
+    must_signin_to_create   :"Vous devez être identifié (ou inscrit) pour ouvrir ou créer une feuille de route.",
+    how_to_make_a_good_nom  :"Pour un nom valide, n'utilisez ni espace, ni caractères spéciaux. Si vous voulez ouvrir une de vos feuilles de route, utilisez le menu, plutôt.",
+    creating                :"Création de la feuille de route…",
+    saving                  :"Sauvegarde en cours…",
+    created                 :"Feuille de route créée avec succès !",
+    loaded                  :"Feuille de route ouverte avec succès !",
+    saved                   :"Feuille de route sauvée avec succès !",
+    ready                   :"La nouvelle feuille de route est prête",
+    no_config_generale      :"Pas de configuration générale pour cette feuille de route"
   },
   // EXERCICES
   Exercices: {
     Config:{
-      saved     : "Configuration des exercices sauvée avec succès.",
+      saved     : "Configuration des exercices sauvée avec succès."
     },
   },
   // EXERCICE
