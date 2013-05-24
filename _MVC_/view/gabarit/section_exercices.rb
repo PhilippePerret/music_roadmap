7732e5793474cdcfc7845b34e897b2437dface18
PhilippePerret <philippe.perret@yahoo.fr>
2013-05-24 06:36:37 +0200

    Refactorisation des fichiers vues

--- _MVC_/view/gabarit/section_exercices.rb
+++ _MVC_/view/gabarit/section_exercices.rb
@@ -12,13 +12,17 @@ html = ""
 # --- LES EXERCICES ---
 html << '<section id="section_exercices">'
 # Boutons
-html << File.read(File.join(FOLDER_VIEWS, 'gabarit', 'boutons_exercices.html'))
+html << Html::load_view('gabarit/boutons_exercices.html')
+# Boite extrait
+html << Html::load_view('gabarit/extrait_partition.html')
 # Création édition
-html << File.read(File.join(FOLDER_VIEWS, 'gabarit', 'form_exercice.html'))
+html << Html::load_view('gabarit/form_exercice.html')
+# Boite pour la BDE
+html << Html::load_view('gabarit/dbe.html')
 # Section pour la gestion des séances de travail
-html << File.read(File.join(FOLDER_VIEWS, 'gabarit', 'seance.html'))
+html << Html::load_view('gabarit/seance.html')
 # Section pour la gestion des rapports
-html << eval(File.read File.join(FOLDER_VIEWS, 'gabarit', 'rapport.rb'))
+html << Html::load_view('gabarit/rapport.rb')
 # UL de la liste des exercices
 html << '<ul id="exercices"></ul>'
 html << '</section>' # section_exercices
